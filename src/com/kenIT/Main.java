c2b58f19a55db525915ab27a3cfcdf5eb75e385e
- <=>
2022-04-13 22:40:07 +0700

    first commit

--- src/com/kenIT/Main.java
+++ src/com/kenIT/Main.java
@@ -3,6 +3,14 @@ package com.kenIT;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Main thread running... ");
+        RunnableDemo runnableDemo = new RunnableDemo("Thread-1-HR-Database");
+        runnableDemo.start();
+
+        RunnableDemo runnableDemo1 = new RunnableDemo("Thread-2-Send-Email");
+        runnableDemo1.start();
+
+        System.out.println("Main thread stopped!!! ");
+
     }
 }
